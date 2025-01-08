a8652501b88d1704c1eab3dff36adf6081f00cf9
AbdullahA <46983048+AbdullahAGit@users.noreply.github.com>
2025-01-08 02:52:19 -0500

    Update registers_testbench.v

--- registers_testbench.v
+++ registers_testbench.v
@@ -1,24 +1,4 @@
 `timescale 1ns/1ps
-///////////////////`///////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/09/2024 10:17:39 PM
-// Design Name: 
-// Module Name: registers_testbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module registers_testbench();
     reg clk;
